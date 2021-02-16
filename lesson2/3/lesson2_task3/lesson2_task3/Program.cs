e4bc0811ecf0e9c4f89e080475e91a201f81171a
Виктория Шумская <overveg@gmail.com>
2021-02-16 18:17:54 +0300

    lesson2 task 3

---
+++ lesson2/3/lesson2_task3/lesson2_task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2_task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //3.Определить, является ли введённое пользователем число чётным.
+            Console.Write("Введите любое число:");
+            
+            int input = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine(input % 2 == 0 ? "Четное" : "Нечетное");
+
+        }
+    }
+}
