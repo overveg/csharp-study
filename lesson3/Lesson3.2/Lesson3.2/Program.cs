3a47a5a84b0a3d0b706da23a5d46f5c5646c161f
Виктория Шумская <overveg@gmail.com>
2021-02-23 14:38:40 +0300

    задача 2

---
+++ lesson3/Lesson3.2/Lesson3.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.Написать программу «Телефонный справочник»: создать двумерный массив 5х2, хранящий список телефонных контактов: первый элемент хранит имя контакта, второй — номер телефона/ email.
+
+            string[,] tel = {
+            {"Егор", "e@mail.ru" },
+            {"Руслан", "r@mail.ru" },
+            {"Маша", "m@mail.ru" },
+            {"Даша", "d@mail.ru" },
+            {"Саша", "s@mail.ru" }
+            };
+            for (int i = 0; i < tel.GetLength(0); i++)
+            {
+                //выводим все поля 
+                Console.Write($"{i}) ");
+                for (int k = 0; k < tel.GetLength(1); k++)
+                {
+                    Console.Write($"{tel[i, k]} ");
+                }
+
+                //выводим по-другому
+                //Console.Write($"{i}) {tel[i, 0]}  ({tel[i, 1]})");
+
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
