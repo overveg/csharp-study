2f7a443c82e5ba64219de7a57d0e346857eb5707
Виктория Шумская <overveg@gmail.com>
2021-02-25 18:33:30 +0300

    задача 4

---
+++ lesson4/Lesson4.4/Lesson4.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4. (*) Написать программу, вычисляющую число Фибоначчи для заданного значения рекурсивным способом.
+            Console.WriteLine("Введите число");
+            int element = Convert.ToInt32(Console.ReadLine());
+            getFibonacci(element);
+            Console.WriteLine(getFibonacci(element));
+        }
+        static int getFibonacci(int element)
+        {
+            return element <= 1 ? element : getFibonacci(element - 1) + getFibonacci(element - 2);
+        }
+    }
+}
