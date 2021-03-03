18fd233c10bc77821f702745f0834ba05bed682c
Виктория Шумская <overveg@gmail.com>
2021-03-03 16:37:55 +0300

    задача 4

---
+++ lesson5/Lesson5.4/Lesson5.4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string workDir = @"d:\Projects\viktoria\test\";
+            string[] entries = Directory.GetFileSystemEntries(workDir, "*", SearchOption.AllDirectories);
+            int entriesLength = entries.Length - 1;
+
+            //с рекурсией
+            Console.WriteLine("с рекурсией");
+            GetCatalog(entriesLength, entries);
+            Console.WriteLine("");
+
+            //без рекурсии
+            Console.WriteLine("без рекурсии");
+            for (int i = 0; i < entries.Length; i++)
+            {
+                Console.Write($">{new DirectoryInfo(entries[i]).Name}");
+            }
+            Console.WriteLine("");
+        }
+
+        static string GetCatalog(int index, string[] array)
+        {
+            //с конца строки
+            if (index >= 0)
+            {
+                Console.Write($"{new DirectoryInfo(array[index]).Name}>");
+                return GetCatalog(index - 1, array);
+            }
+
+            return "";
+        }
+    }
+}
