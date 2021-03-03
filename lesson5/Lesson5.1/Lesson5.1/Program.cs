d5d4cde3d94a3c626cb1492c7014ca6d4cd990da
Виктория Шумская <overveg@gmail.com>
2021-03-03 09:29:21 +0300

    задача 1

---
+++ lesson5/Lesson5.1/Lesson5.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Lesson5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+            Console.WriteLine("Введите строку:");
+            string userInput = Console.ReadLine();
+
+            string filename = "text.txt";
+            File.WriteAllText(filename, userInput);
+        }
+
+    }
+}
